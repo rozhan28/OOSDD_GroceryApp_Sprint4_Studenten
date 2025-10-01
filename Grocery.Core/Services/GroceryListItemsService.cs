1c8241d37b8657ecd9e82e53493dc4ddcea4618d
rozhan28 <rozhanarya1@gmail.com>
2025-10-01 22:02:23 +0200

    Usercase 11 best verkochte producten toegevoegd

--- Grocery.Core/Services/GroceryListItemsService.cs
+++ Grocery.Core/Services/GroceryListItemsService.cs
@@ -9,7 +9,9 @@ namespace Grocery.Core.Services
         private readonly IGroceryListItemsRepository _groceriesRepository;
         private readonly IProductRepository _productRepository;
 
-        public GroceryListItemsService(IGroceryListItemsRepository groceriesRepository, IProductRepository productRepository)
+        public GroceryListItemsService(
+            IGroceryListItemsRepository groceriesRepository,
+            IProductRepository productRepository)
         {
             _groceriesRepository = groceriesRepository;
             _productRepository = productRepository;
@@ -17,14 +19,18 @@ namespace Grocery.Core.Services
 
         public List<GroceryListItem> GetAll()
         {
-            List<GroceryListItem> groceryListItems = _groceriesRepository.GetAll();
+            var groceryListItems = _groceriesRepository.GetAll();
             FillService(groceryListItems);
             return groceryListItems;
         }
 
         public List<GroceryListItem> GetAllOnGroceryListId(int groceryListId)
         {
-            List<GroceryListItem> groceryListItems = _groceriesRepository.GetAll().Where(g => g.GroceryListId == groceryListId).ToList();
+            var groceryListItems = _groceriesRepository
+                .GetAll()
+                .Where(g => g.GroceryListId == groceryListId)
+                .ToList();
+
             FillService(groceryListItems);
             return groceryListItems;
         }
@@ -51,12 +57,42 @@ namespace Grocery.Core.Services
 
         public List<BestSellingProducts> GetBestSellingProducts(int topX = 5)
         {
-            throw new NotImplementedException();
+            var allItems = _groceriesRepository.GetAll();
+            var allProducts = _productRepository.GetAll().ToDictionary(p => p.Id);
+
+            var bestSelling = allItems
+                .GroupBy(i => i.ProductId)
+                .Select(g =>
+                {
+                    var productInfo = allProducts.ContainsKey(g.Key)
+                        ? allProducts[g.Key]
+                        : new Product(0, "Onbekend product", 0);
+
+                    return new
+                    {
+                        ProductId = g.Key,
+                        productInfo.Name,
+                        productInfo.Stock,
+                        NrOfSells = g.Sum(i => i.Amount) 
+                    };
+                })
+                .OrderByDescending(p => p.NrOfSells)
+                .Take(topX)
+                .Select((p, index) => new BestSellingProducts(
+                    p.ProductId,
+                    p.Name,
+                    p.Stock,
+                    p.NrOfSells,
+                    index + 1 
+                ))
+                .ToList();
+
+            return bestSelling;
         }
 
         private void FillService(List<GroceryListItem> groceryListItems)
         {
-            foreach (GroceryListItem g in groceryListItems)
+            foreach (var g in groceryListItems)
             {
                 g.Product = _productRepository.Get(g.ProductId) ?? new(0, "", 0);
             }
